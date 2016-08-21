3dc0f74bbcdfd698e3a3304d3a62e237f1f0f772
wcchang <wcc689653@gmail.com>
2016-08-21 09:46:19 +0800

    The first Cool Thing.

---
+++ app/src/main/java/com/example/wcchang/gitpractice/GitPractice.java
@@ -0,0 +1,13 @@
+package com.example.wcchang.gitpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitPractice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_practice);
+    }
+}
